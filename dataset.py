69f7d1992075a40c5e7793cc01b228bbdcbcedb0
Jimmy <chengjie.zheng668@gmail.com>
2023-03-05 01:09:43 -0500

    update dataset.py

--- dataset.py
+++ dataset.py
@@ -13,6 +13,10 @@ mean = np.array([0.485, 0.456, 0.406])
 std = np.array([0.229, 0.224, 0.225])
 
 
+import sys
+
+
+
 class Dataset(Dataset):
     def __init__(self, dataset_path, split_path, split_number, input_shape, sequence_length, training):
         self.training = training
@@ -29,7 +33,7 @@ class Dataset(Dataset):
             ]
         )
 
-    def _extract_label_mapping(self, split_path="data/miceTrainTestlist"):
+    def _extract_label_mapping(self, split_path="data/ucfTrainTestlist"):
         """ Extracts a mapping between activity name and softmax index """
         with open(os.path.join(split_path, "classInd.txt")) as file:
             lines = file.read().splitlines()
@@ -40,10 +44,10 @@ class Dataset(Dataset):
         return label_mapping
 
     def _extract_sequence_paths(
-        self, dataset_path, split_path="data/miceTrainTestlist", split_number=1, training=True
+        self, dataset_path, split_path="data/ucfTrainTestlist", split_number=1, training=True
     ):
         """ Extracts paths to sequences given the specified train / test split """
-        assert split_number in [1], "Split number has to be one of {1, 2, 3}"
+        assert split_number in [1, 2, 3], "Split number has to be one of {1, 2, 3}"
         fn = f"trainlist0{split_number}.txt" if training else f"testlist0{split_number}.txt"
         split_path = os.path.join(split_path, fn)
         with open(split_path) as file:
@@ -64,8 +68,9 @@ class Dataset(Dataset):
 
     def _pad_to_length(self, sequence):
         """ Pads the sequence to required sequence length """
-        print("测试1", sequence)
+        ### print("测试1", sequence)
         left_pad = sequence[0]
+        ### print("test2", left_pad)
         if self.sequence_length is not None:
             while len(sequence) < self.sequence_length:
                 sequence.insert(0, left_pad)
@@ -73,18 +78,25 @@ class Dataset(Dataset):
 
     def __getitem__(self, index):
         sequence_path = self.sequences[index % len(self)]
+        ### print("test1", self.sequences, index)
+
         ### print("测试4", sequence_path)
         # Sort frame sequence based on frame number
+        # print(os.listdir(sequence_path))
         ### print("测试3", glob.glob(f"{sequence_path}/*.jpg"))
         image_paths = sorted(glob.glob(f"{sequence_path}/*.jpg"), key=lambda path: self._frame_number(path))
         # Pad frames sequences shorter than `self.sequence_length` to length
-        print("测试2", image_paths)
+        # if not image_paths: 1/0
+        #print("测试2", image_paths)
         image_paths = self._pad_to_length(image_paths)
         if self.training:
             # Randomly choose sample interval and start frame
             sample_interval = np.random.randint(1, len(image_paths) // self.sequence_length + 1)
+            ### print("随机数", sample_interval, self.sequence_length, len(image_paths))
             start_i = np.random.randint(0, len(image_paths) - sample_interval * self.sequence_length + 1)
+            ### print("start_i", start_i)
             flip = np.random.random() < 0.5
+            ### print("flip", flip)
         else:
             # Start at first frame and sample uniformly over sequence
             start_i = 0
