7322875b65e26b0caf6844bfefe6f3b2ff972fa9
Jimmy <chengjie.zheng668@gmail.com>
2023-03-05 00:35:11 -0500

    add .gitgnore file

--- data/extract_frames.py
+++ data/extract_frames.py
@@ -28,7 +28,7 @@ if __name__ == "__main__":
     time_left = 0
     video_paths = glob.glob(os.path.join(opt.dataset_path, "*", "*.mp4"))
     for i, video_path in enumerate(video_paths):
-        sequence_type, sequence_name = video_path.split(".avi")[0].split("/")[-2:]
+        sequence_type, sequence_name = video_path.split(".mp4")[0].split("/")[-2:]
         sequence_path = os.path.join(f"{opt.dataset_path}-frames", sequence_type, sequence_name)
 
         if os.path.exists(sequence_path):
